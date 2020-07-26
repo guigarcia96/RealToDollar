03a1812d6f47a60b0a2ac9d24661abb719f5c470
Guilherme Moraes <gmoraes1@outlook.com>
2020-07-26 14:39:58 -0300

    remoção de Adsense e Refatoração de código

--- src/js/index.js
+++ src/js/index.js
@@ -2,50 +2,48 @@ let resultDiv = document.querySelector('.result')
 let h1 = document.querySelector('#finalResult')
 let valueButton = document.querySelector('#button')
 
+function insertValueInHTML(digitedValue, convertedValue, digitedValueConverted, response) {
+
+  let dollarValue = response
 
 
-function insertValueInHTML(digitedValue, convertedValue, digitedValueConverted) {
-  axios
-    .get('https://free.currconv.com/api/v7/convert?q=BRL_USD&compact=ultra&apiKey=290f5fe71e4298e51733')
-    .then(function (response) {
-      // handle success
-      const data = response.data
-      convertedValue = (digitedValueConverted * data.BRL_USD)
+  convertedValue = (digitedValueConverted * dollarValue.BRL_USD)
 
 
-      if (convertedValue.toString() === 'NaN') {
-        alert('Favor digitar apenas numeros')
-        digitedValue.value = ''
+  if (convertedValue.toString() === 'NaN') {
+    alert('Favor digitar apenas numeros')
+    digitedValue.value = ''
 
 
-      } else {
+  } else {
 
-        h1.innerHTML = digitedValueConverted > 1 ?
-          `R$${digitedValueConverted} em Dollar são $${convertedValue.toFixed(2)}` :
-          `R$${digitedValueConverted} em Dollar é $${convertedValue.toFixed(2)}`
-        digitedValue.value = ''
-      }
 
-    })
+    h1.innerHTML = digitedValueConverted > 1 ?
+      `R$${String(digitedValueConverted.toFixed(2)).replace('.', ',')} em Dollar são $${String(convertedValue.toFixed(2)).replace('.', ',')}` :
+      `R$${String(digitedValueConverted.toFixed(2)).replace('.', ',')} em Dollar é $${String(convertedValue.toFixed(2)).replace('.', ',')}`
+    digitedValue.value = ''
+  }
+
+
 }
 
 const converteValor = () => {
 
-  let digitedValue = document.querySelector('#valor')
-  let digitedValueConverted = parseFloat(digitedValue.value)
-  let convertedValue
+  axios.get('https://free.currconv.com/api/v7/convert?q=BRL_USD&compact=ultra&apiKey=290f5fe71e4298e51733')
+    .then(response => {
 
-  insertValueInHTML(digitedValue, convertedValue, digitedValueConverted)
+      valueButton.addEventListener('click', () => {
+        event.preventDefault()
+        let digitedValue = document.querySelector('#valor')
+        let digitedValueConverted = digitedValue.value
+        digitedValueConverted = digitedValueConverted.replace(',', '.')
+        digitedValueConverted = parseFloat(digitedValueConverted)
+        let convertedValue
+        insertValueInHTML(digitedValue, convertedValue, digitedValueConverted, response.data)
+
+
+
+      })
+    })
 }
-
-
-
-
-valueButton.addEventListener('click', () => {
-  event.preventDefault()
-  converteValor()
-
-
-
-})
-
+converteValor()
